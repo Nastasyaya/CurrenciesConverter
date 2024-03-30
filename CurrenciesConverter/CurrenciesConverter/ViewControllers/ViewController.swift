c78a39b81b5c562db20db1409db91e03ee5e75f5
Anastasiia <nastyak25555@gmail.com>
2024-03-30 08:33:44 +0100

    Folder structures clean-up

---
+++ CurrenciesConverter/CurrenciesConverter/ViewControllers/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  CurrenciesConverter
+//
+//  Created by Анастасия Кутняхова on 30.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
